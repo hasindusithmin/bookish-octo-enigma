fd64c74a12de149ec13c54d25b6812701f526ea0
hasindu sithmin <hasindusithmin64@gmail.com>
2022-08-07 10:21:50 +0000

    create statics method

--- models/userModel.js
+++ models/userModel.js
@@ -37,6 +37,16 @@ userSchema.pre('save',async function(next) {
     next()
 })
 
+userSchema.statics.login = async function(email,password){
+    const user = await this.findOne({email})
+    if(user){
+        const auth = await bcrypt.compare(password,user.password)
+        if(auth) return user
+        throw Error("Incorrect Password")
+    }
+    throw Error("User not found")
+}
+
 const userModel = model('employee',userSchema)
 
 export default userModel;
