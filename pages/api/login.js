f61a4a11c595dfd51f8c30e8a76412ae16e650a4
hasindu sithmin <hasindusithmin64@gmail.com>
2022-08-07 10:31:13 +0000

    create /api/login function

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,5 +1,18 @@
-
-
+import connectMongo from "../../utils/connectMongo"
+import userModel from "../../models/userModel";
+import jwt from "jsonwebtoken"
+import {serialize} from "cookie"
 export default async function handler(req,res){
+    await connectMongo()
     const {email,password} = req.body;
+    try {
+        const user = await userModel.login(email,password)
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:3600})
+        res.setHeader('Set-Cookie', serialize('token', token, { httpOnly:true,maxAge:3600 }));
+        res.status(200).json({user:user._id})
+    } catch (error) {
+        res.status(401).json({error:error.message})
+    }
+
+
 }
--- pages/api/signup.js
+++ pages/api/signup.js
@@ -9,7 +9,7 @@ export default async function handler(req,res){
         const user = await userModel.create({name,email,password,nic,post,contact})
         const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:3600})
         res.setHeader('Set-Cookie', serialize('token', token, { httpOnly:true,maxAge:3600 }));
-        res.status(201).json({user:user._id})
+        res.status(200).json({user:user._id})
     } catch (error) {
         res.status(401).json({error:error.message})
     }
