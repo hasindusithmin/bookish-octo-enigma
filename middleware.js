c92f00300b4556d1c2da04c9995386be360e73cb
hasindu sithmin <hasindusithmin64@gmail.com>
2022-08-17 09:45:17 +0000

    Delete->jsonwebtoken-esm & Add->crypto-js

--- middleware.js
+++ middleware.js
@@ -4,32 +4,31 @@ import jwt from "jsonwebtoken-esm"
 // then we'll rewrite the request to /beta
 export function middleware(req) {
 
-    if(req.nextUrl.pathname.startsWith('/')) {
-      const token = req.cookies.get('token')
-      if(token) {
-        jwt.verify(token,process.env.JWT_SECRET,(err,decodeToken)=>{
-          if (err) return NextResponse.next();
-          else {
-            console.log(decodeToken);
-          }
-        })
-      }
-      else {
-        return NextResponse.next();
-      }
+  if (req.nextUrl.pathname.startsWith('/onauth')) {
+    const token = req.cookies.get('token')
+    const url = req.nextUrl.clone()
+    url.pathname = '/signin'
+    if (token) {
+      jwt.verify(token, process.env.JWT_SECRET, (err, decodeToken) => {
+        if (err) NextResponse.redirect(url)
+        else return NextResponse.next()
+      })
     }
-    if(req.nextUrl.pathname.startsWith('/onauth')) {
-      const token = req.cookies.get('token')
-      const url = req.nextUrl.clone()
-      url.pathname = '/signin'
-      if(token) {
-        jwt.verify(token,process.env.JWT_SECRET,(err,decodeToken)=>{
-          if (err) return NextResponse.redirect(url);
-          else return NextResponse.next()
-        })
-      }
-      else {
-        return NextResponse.redirect(url);
-      }
+    else NextResponse.redirect(url)
+  }
+  if (!req.nextUrl.pathname.startsWith('/onauth')) {
+    const token = req.cookies.get('token')
+    if (token) {
+      jwt.verify(token, process.env.JWT_SECRET, (err, decodeToken) => {
+        if (err) return NextResponse.next();
+        else {
+          console.log(decodeToken);
+        }
+      })
     }
+    else {
+      return NextResponse.next();
+    }
+  }
+
 }
